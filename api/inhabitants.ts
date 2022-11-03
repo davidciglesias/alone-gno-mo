464ffe2c2102d86f84da66aaddc8648804d6495c
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-11-03 16:06:41 +0100

    👽️ Add getInhabitants request

---
+++ api/inhabitants.ts
@@ -0,0 +1,76 @@
+import { toast } from "react-toastify";
+
+export interface InhabitantAM {
+  id: string;
+  name: string;
+  thumbnail: string;
+  age: number;
+  weight: number;
+  height: number;
+  hair_color: string;
+  professions: string[];
+  friends: string[];
+}
+
+export interface InhabitantVM {
+  id: string;
+  name: string;
+  thumbnail: string;
+  age: number;
+  weight: number;
+  height: number;
+  hairColor: string;
+  professions: string[];
+  friends: string[];
+}
+
+export interface InhabitantsAM {
+  Brastlewark: InhabitantAM[];
+}
+
+export function mapInhabitantsAMToVM({
+  Brastlewark,
+}: InhabitantsAM): InhabitantVM[] {
+  return Brastlewark.map(
+    ({
+      id,
+      name,
+      thumbnail,
+      age,
+      weight,
+      height,
+      hair_color,
+      professions,
+      friends,
+    }) => ({
+      id,
+      name,
+      thumbnail,
+      age,
+      weight,
+      height,
+      hairColor: hair_color,
+      professions,
+      friends,
+    }),
+  );
+}
+
+export default async function getInhabitants() {
+  const response = await window.fetch(
+    "https://raw.githubusercontent.com/davidciglesias/alone-gno-mo/main/data/brastlewark.json",
+  );
+
+  const data = await response.json();
+
+  if (response.ok) {
+    return Promise.resolve(mapInhabitantsAMToVM(data as InhabitantsAM));
+  } else {
+    // TODO Add external logging to understand why this fetch failed
+    toast.error(
+      "Sorry, the inhabitants must be at the tabern. Please try again later!",
+      { icon: "😥" },
+    );
+    return [];
+  }
+}
