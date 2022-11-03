da5858275a47821e7938a5ac0583415051783c21
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-11-03 21:23:57 +0100

    💄 Improve visuals of the Inhabitant Dialog

--- components/InhabitantDialog.tsx
+++ components/InhabitantDialog.tsx
@@ -1,42 +1,26 @@
-import {
-  Card,
-  CardContent,
-  Dialog,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
+import BadgeIcon from "@mui/icons-material/Badge";
+import CakeIcon from "@mui/icons-material/Cake";
+import Face3Icon from "@mui/icons-material/Face3";
+import HeightIcon from "@mui/icons-material/Height";
+import ScaleIcon from "@mui/icons-material/Scale";
+import { Card, CardContent, Dialog, Grid, Stack } from "@mui/material";
 
 import Image from "next/image";
 
 import { InhabitantVM } from "api/inhabitants";
 import loadingImage from "public/alone-gno-mo.png";
 
+import ColorComponent from "./ColorComponent";
+import LabelItem from "./LabelItem";
+
 interface Props {
   inhabitant: InhabitantVM;
   onDialogClose: () => void;
 }
 
-function LabelItem({ gridArea, label, children }: any) {
-  return (
-    <Grid item gridArea={gridArea} direction="column">
-      <Typography variant="h6">{label}</Typography>
-      {children}
-    </Grid>
-  );
-}
-
 export default function InhabitantDialog({ onDialogClose, inhabitant }: Props) {
-  const {
-    thumbnail,
-    name,
-    age,
-    weight,
-    height,
-    hairColor,
-    professions,
-    friends,
-  } = inhabitant;
+  const { thumbnail, name, age, weight, height, hairColor } = inhabitant;
+
   return (
     <Dialog
       open={true}
@@ -70,20 +54,36 @@ export default function InhabitantDialog({ onDialogClose, inhabitant }: Props) {
 							"age		    weight"
 							"height	    hair-color"`}
             >
-              <LabelItem gridArea="full-name" label="Full name">
-                {name}
+              <LabelItem
+                gridArea="full-name"
+                label="Full name"
+                icon={<BadgeIcon />}
+              >
+                <div>{name}</div>
               </LabelItem>
-              <LabelItem gridArea="age" label="Age">
-                {age}
+              <LabelItem gridArea="age" label="Age" icon={<CakeIcon />}>
+                <div>{age}</div>
               </LabelItem>
-              <LabelItem gridArea="weight" label="Weight">
-                {weight}
+              <LabelItem
+                gridArea="weight"
+                label="Weight (kg)"
+                icon={<ScaleIcon />}
+              >
+                <div>{weight}</div>
               </LabelItem>
-              <LabelItem gridArea="height" label="Height">
-                {height}
+              <LabelItem
+                gridArea="height"
+                label="Height (cm)"
+                icon={<HeightIcon />}
+              >
+                <div>{height}</div>
               </LabelItem>
-              <LabelItem gridArea="hair-color" label="Hair Color">
-                {hairColor}
+              <LabelItem
+                gridArea="hair-color"
+                label="Hair Color"
+                icon={<Face3Icon />}
+              >
+                <ColorComponent textColor={hairColor} />
               </LabelItem>
             </Grid>
           </Stack>
---
+++ components/LabelItem.tsx
@@ -0,0 +1,33 @@
+import { Grid, Stack, Typography } from "@mui/material";
+
+interface Props {
+  gridArea: string;
+  label: string;
+  children: JSX.Element;
+  icon: JSX.Element;
+}
+
+export default function LabelItem({ gridArea, label, children, icon }: Props) {
+  return (
+    <Grid
+      item
+      gridArea={gridArea}
+      direction="column"
+      display="flex"
+      gap={1}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction="row"
+        gap={1}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {icon}
+        <Typography variant="h6">{label}</Typography>
+      </Stack>
+      {children}
+    </Grid>
+  );
+}
