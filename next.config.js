7b122cc698d092e3c14924a1f5c377aa41ec15c8
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-11-03 19:51:49 +0100

    🔧 Add publicdomainpictures to allowed images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "www.publicdomainpictures.net",
+        pathname: "/pictures/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.publicdomainpictures.net",
+        pathname: "/pictures/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
